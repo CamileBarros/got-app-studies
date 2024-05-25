ea8f5f9ad045390c3292ab0595ea7b5f2f338cee
CamileBarros <camile.barros@avenue.us>
2024-05-25 20:35:21 -0300

    Adiciona bullets

--- lib/features/houses_list/presentation/widgets/page_view_houses_card.dart
+++ lib/features/houses_list/presentation/widgets/page_view_houses_card.dart
@@ -20,55 +20,84 @@ class PageViewHousesCard extends StatelessWidget {
             child: Text('No houses available'),
           );
         }
-        return PageView(
-            controller: gotHousesStateBackup.pageController.value,
-            onPageChanged: gotHousesStateBackup.onPageChanged,
-            children: gotHousesStateBackup.housesList.value.map((house) {
-              return SizedBox(
-                height: MediaQuery.sizeOf(context).height / 3,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'Game of Thrones\nHouses',
-                        style: TextStyle(
-                          fontSize: 32,
-                          fontFamily: 'Cinzel',
-                          fontWeight: FontWeight.bold,
+        return Column(
+          children: [
+            Flexible(
+              child: PageView(
+                  controller: gotHousesStateBackup.pageController.value,
+                  onPageChanged: gotHousesStateBackup.onPageChanged,
+                  children: gotHousesStateBackup.housesList.value.map((house) {
+                    return SizedBox(
+                      height: MediaQuery.sizeOf(context).height / 3,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 24),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Game of Thrones\nHouses',
+                              style: TextStyle(
+                                fontSize: 32,
+                                fontFamily: 'Cinzel',
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 16),
+                            const Text(
+                              'Das terras geladas do Norte às ensolaradas\nde Dorne, a complexa rede de alianças e\nrivalidades determina o destino dos reinos.\n\nDescubra mais sobre as famílias nobres que\ndisputam o Trono de Ferro e desvende os\nsegredos por trás de suas histórias.',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontFamily: 'RobotoSlab',
+                              ),
+                            ),
+                            const SizedBox(height: 32),
+                            CardButtonHouses(
+                              size: EducationalBannerSize.medium,
+                              onTap: () {
+                                Navigator.of(context).pushNamed(
+                                  '/house_list_details',
+                                  arguments: {
+                                    'membersHouse': house.members,
+                                    'imageCharacters': gotHousesStateBackup
+                                        .imageCharacters.value,
+                                    'title': house.name,
+                                  },
+                                );
+                              },
+                              description: house.name,
+                              colorBanner1: const Color.fromARGB(255, 1, 59, 106),
+                              colorBanner2:
+                                  const Color.fromARGB(255, 155, 200, 237),
+                            ),
+                          ],
                         ),
                       ),
-                      const SizedBox(height: 16),
-                      const Text(
-                        'Das terras geladas do Norte às ensolaradas\nde Dorne, a complexa rede de alianças e\nrivalidades determina o destino dos reinos.\n\nDescubra mais sobre as famílias nobres que\ndisputam o Trono de Ferro e desvende os\nsegredos por trás de suas histórias.',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontFamily: 'RobotoSlab',
+                    );
+                  }).toList()),
+            ),
+            ValueListenableBuilder<int>(
+              valueListenable: gotHousesStateBackup.currentPage,
+              builder: (context, value, child) {
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 300),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(gotHousesStateBackup.housesList.value.length, (index) {
+                      return Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 4),
+                        width: 8,
+                        height: 8,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: value == index ? Colors.blue : Colors.grey,
                         ),
-                      ),
-                      const SizedBox(height: 32),
-                      CardButtonHouses(
-                        size: EducationalBannerSize.medium,
-                        onTap: () {
-                          Navigator.of(context).pushNamed(
-                            '/house_list_details',
-                            arguments: {
-                              'membersHouse': house.members,
-                              'imageCharacters':
-                                  gotHousesStateBackup.imageCharacters.value,
-                              'title': house.name,
-                            },
-                          );
-                        },
-                        description: house.name,
-                        colorBanner1: const Color.fromARGB(255, 1, 59, 106),
-                        colorBanner2: const Color.fromARGB(255, 155, 200, 237),
-                      ),
-                    ],
+                      );
+                    }),
                   ),
-                ),
-              );
-            }).toList());
+                );
+              },
+            ),
+          ],
+        );
       });
 }
